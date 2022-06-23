3b916b49095631a6c46f66d968f1d26cdef35ecd
Rethink <rethink@MacBook-Pro-de-Rethink-2.local>
2022-06-23 15:02:27 -0300

    refactor: move SUT creation to a setup method

--- treinamento_flutter/test/data/usecases/remote_authentication_test.dart
+++ treinamento_flutter/test/data/usecases/remote_authentication_test.dart
@@ -24,17 +24,21 @@ class RemoteAuthentication{
 }
  class HttpClientSpy extends Mock implements HttpClient {}
 void main(){
-  test('Should call HttpClient with correct URL', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient : httpClient , url : url);  //system under test = sut
+  RemoteAuthentication sut;
+  HttpClientSpy httpClient;
+  String url;
 
+  setUp((){
+   httpClient = HttpClientSpy();
+   url = faker.internet.httpUrl();
+   sut = RemoteAuthentication(httpClient : httpClient , url : url);  //system under test = sut
+  });
+  test('Should call HttpClient with correct URL', () async {
     await sut.auth();
 
     verify(httpClient.request(
       url: url,
       method : 'post'
       ));
-
   });
 }

6fdaa4c4b9a7b7a963e603ebb9a1ab8049c54b3d
Rethink <rethink@mbpderethink2.lan>
2022-06-27 10:56:53 -0300

    refactor/test: move production code to their target

--- treinamento_flutter/test/data/usecases/remote_authentication_test.dart
+++ treinamento_flutter/test/data/usecases/remote_authentication_test.dart
@@ -1,29 +1,13 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
 import 'package:treinamento_flutter/domain/usescases/authentication.dart';
+import 'package:treinamento_flutter/data/http/http.dart';
+import 'package:treinamento_flutter/data/usecases/usecases.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
 
-  RemoteAuthentication({@required this.httpClient, @required this.url});
 
-  Future<void> auth(AuthenticationParams params) async {
-    final body = {'email': params.email , 'password': params.secret};
-    await httpClient.request(url: url, method: 'post', body: body);
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request({
-    @required String url,
-    @required String method,
-    Map body
-  }) {}
-}
 
 class HttpClientSpy extends Mock implements HttpClient {}
 

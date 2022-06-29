243fc991520d4dd6bbac30b0dc5e1f81936b59ac
Rethink <rethink@mbpderethink2.lan>
2022-06-29 09:25:54 -0300

    ensure HttpAdapter calls post with correct Body

--- treinamento_flutter/test/data/infra/http/htpp_adapter_test.dart
+++ treinamento_flutter/test/data/infra/http/htpp_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -12,12 +14,13 @@ class HttpAdapter {
   Future<Map> request({
     @required String url,
     @required String method,
+    Map body,
   }) async {
     final headers = {
       'content-type': 'aplication/json',
       'accept': 'aplication/json'
     };
-    await client.post(url, headers: headers);
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
@@ -27,7 +30,7 @@ void main() {
   HttpAdapter sut;
   ClientSpy client;
   String url;
-  
+
   setUp(() {
      client = ClientSpy();
      sut = HttpAdapter(client);
@@ -35,12 +38,15 @@ void main() {
   });
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(url: url, method: 'post', body : {'any_key' : 'any_value'});
 
-      verify(client.post(url, headers: {
+      verify(client.post(url, 
+      headers: {
         'content-type': 'aplication/json',
         'accept': 'aplication/json'
-      }));
+      },
+      body: 'body : {"any_key" : "any_value"}',
+      ));
     });
   });
 }

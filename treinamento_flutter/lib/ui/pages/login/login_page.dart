a2060fdc1522656cbe13f929adc96e2ac0363ae2
Rethink <rethink@mbpderethink2.lan>
2022-07-06 14:40:05 -0300

    ensure LoginPage presents error if email is invalid

--- treinamento_flutter/lib/ui/pages/login/login_page.dart
+++ treinamento_flutter/lib/ui/pages/login/login_page.dart
@@ -21,13 +21,19 @@ class LoginPage extends StatelessWidget {
               padding:  EdgeInsets.all(32.0),
               child: Form(
                 child: Column(children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      icon: Icon(Icons.email,color: Theme.of(context).primaryColorLight,),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    onChanged: presenter.validateEmail,
+                  StreamBuilder<String>(
+                    stream: presenter.emailErrorSteam,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          icon: Icon(Icons.email,color: Theme.of(context).primaryColorLight,),
+                          errorText: snapshot.data,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: presenter.validateEmail,
+                      );
+                    }
                   ), 
                   Padding(
                     padding:  EdgeInsets.only(top:8.0,bottom: 32),
--- treinamento_flutter/test/ui/pages/login_page_test.dart
+++ treinamento_flutter/test/ui/pages/login_page_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,13 +11,20 @@ class LoginPresenterSpy extends Mock implements LoginPresenter {}
 
 void main() {
   LoginPresenterSpy presenter;
+  StreamController<String> emailErrorController;
 
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
+    emailErrorController = StreamController<String>();
+
+    when(presenter.emailErrorSteam).thenAnswer((_) => emailErrorController.stream);
+
     final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage); //renderiza estado inicial da pagina
   }
-
+  tearDown((){
+    emailErrorController.close();
+  });
   testWidgets('Should load with correct initial state',
       (WidgetTester tester) async {
     await loadPage(tester);
@@ -48,4 +57,14 @@ void main() {
     await tester.enterText(find.bySemanticsLabel('Senha'), password);
     verify(presenter.validatePassword(password));
   });
+  testWidgets('Should present error if email is invalid',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    emailErrorController.add('any error');
+    await tester.pump();
+
+    expect(find.text('any error'), findsOneWidget);
+  });
+
 }

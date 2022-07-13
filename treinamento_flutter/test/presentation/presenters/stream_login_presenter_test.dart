f67da58e7da991c62f019d8fb2bcf5ea47eb37e8
Rethink <rethink@mbpderethink2.lan>
2022-07-13 10:52:08 -0300

    ensure StreamLoginPresenter emits error if email is invalid

--- treinamento_flutter/test/presentation/presenters/stream_login_presenter_test.dart
+++ treinamento_flutter/test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,19 +1,31 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:treinamento_flutter/presentation/protocols/validation.dart';
 
-abstract class Validation {
-  String validate({@required String field, @required String value});
+
+class LoginState {
+  String emailError;
 }
 
 class StreamLoginPresenter {
   final Validation validation;
 
+  final _controller = StreamController<LoginState>.broadcast();
+
+  var _state = LoginState();
+
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
   StreamLoginPresenter({@required this.validation});
 
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
@@ -24,10 +36,11 @@ void main() {
   ValidationSpy validation;
   String email;
 
-  PostExpectation mockValiditionCall(String field) =>  
-    when(validation.validate(field: field == null ? anyNamed('field'): field, value: anyNamed('value')));
+  PostExpectation mockValiditionCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')));
 
-  void mockValidation ({String field, String value}){
+  void mockValidation({String field, String value}) {
     mockValiditionCall(field).thenReturn(value);
   }
 
@@ -43,9 +56,11 @@ void main() {
     verify(validation.validate(field: 'email', value: email)).called(1);
   });
 
-   test('Should emit email error if validation fails', () {
-    sut.validateEmail(email);
+  test('Should emit email error if validation fails', () {
+    mockValidation(value: 'error');
 
-    verify(validation.validate(field: 'email', value: email)).called(1);
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }

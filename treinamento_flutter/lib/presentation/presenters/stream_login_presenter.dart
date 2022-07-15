c58f929a50b341ecbc3d031bde777552a10b1f60
Rethink <rethink@mbpderethink2.lan>
2022-07-15 10:29:16 -0300

    ensure StreamLoginPresenter emits from valid event if form is valid

--- treinamento_flutter/lib/presentation/presenters/stream_login_presenter.dart
+++ treinamento_flutter/lib/presentation/presenters/stream_login_presenter.dart
@@ -5,10 +5,16 @@ import 'package:meta/meta.dart';
 import '../protocols/protocols.dart';
 
 class LoginState {
+  String email;
+  String password;
   String emailError;
   String passwordError;
 
-  bool get isFormValid => false;
+  bool get isFormValid =>
+      emailError == null &&
+      passwordError == null &&
+      email != null &&
+      password != null;
 }
 
 class StreamLoginPresenter {
@@ -30,14 +36,16 @@ class StreamLoginPresenter {
   StreamLoginPresenter({@required this.validation});
 
   void _update() => _controller.add(_state);
-  
+
   void validateEmail(String email) {
+    _state.email = email;
     _state.emailError = validation.validate(field: 'email', value: email);
     _controller.add(_state);
     _update();
   }
 
   void validatePassword(String password) {
+    _state.password = password;
     _state.passwordError =
         validation.validate(field: 'password', value: password);
     _update();

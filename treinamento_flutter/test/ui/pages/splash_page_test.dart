89464458efa95892c9c994edf49598570c03141c
Rethink <rethink@mbpderethink2.lan>
2022-08-17 10:43:33 -0300

    ensure SplashPage calls loadCurrentAccount on page

--- treinamento_flutter/test/ui/pages/splash_page_test.dart
+++ treinamento_flutter/test/ui/pages/splash_page_test.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get/route_manager.dart';
+import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
+import 'package:meta/meta.dart';
 
 class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter; 
+
+  SplashPage({@required this.presenter});
+
   @override
   Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+    
     return Scaffold(
       appBar: AppBar(title: Text('4Dev')),
       body: Center(
@@ -14,18 +22,38 @@ class SplashPage extends StatelessWidget {
   }
 }
 
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
+
 void main() {
+  SplashPresenterSpy presenter;
+
   Future<void> loadPage(WidgetTester tester) async {
     //Sempre quando formos testar um widget que depende do material design temos que englobar no pumpWidget a widget em um MaterialApp widget. Também vamos usar rotas, entao por isso tbm o get
+
+    presenter = SplashPresenterSpy();
     await tester.pumpWidget(GetMaterialApp(
       initialRoute: '/',
-      getPages: [GetPage(name: '/', page: () => SplashPage())],
+      getPages: [
+        GetPage(name: '/', page: () => SplashPage(presenter: presenter))
+      ],
     ));
   }
 
-  testWidgets('Should present loading(spinner) on page load', (WidgetTester tester) async {
+  testWidgets('Should present loading(spinner) on page load',
+      (WidgetTester tester) async {
     await loadPage(tester);
-    
+
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should call loadCurrentAccount on page load',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadCurrentAccount()).called(1);
   });
 }

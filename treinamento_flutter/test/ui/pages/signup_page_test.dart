bf1a91c657c45c22ad605a02bdd8bdab8f4e6369
Rethink <rethink@mbpderethink2.lan>
2022-08-29 11:18:48 -0300

    ensure SignUpPage present password error

--- treinamento_flutter/test/ui/pages/signup_page_test.dart
+++ treinamento_flutter/test/ui/pages/signup_page_test.dart
@@ -154,4 +154,42 @@ void main() {
             of: find.bySemanticsLabel('Nome'), matching: find.byType(Text)),
         findsOneWidget);
   });
+
+   testWidgets('Should present password error', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add(UIError.invalidField);
+    await tester.pump();
+    expect(find.text('Campo inválido.'), findsOneWidget);
+
+    passwordErrorController.add(UIError.requiredField);
+    await tester.pump();
+    expect(find.text('Campo obrigatório.'), findsOneWidget);
+
+    passwordErrorController.add(null);
+    await tester.pump();
+    expect(
+        find.descendant(
+            of: find.bySemanticsLabel('Senha'), matching: find.byType(Text)),
+        findsOneWidget);
+  });
+
+   testWidgets('Should present password error', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    passwordErrorController.add(UIError.invalidField);
+    await tester.pump();
+    expect(find.text('Campo inválido.'), findsOneWidget);
+
+    passwordErrorController.add(UIError.requiredField);
+    await tester.pump();
+    expect(find.text('Campo obrigatório.'), findsOneWidget);
+
+    passwordErrorController.add(null);
+    await tester.pump();
+    expect(
+        find.descendant(
+            of: find.bySemanticsLabel('Senha'), matching: find.byType(Text)),
+        findsOneWidget);
+  });
 }
